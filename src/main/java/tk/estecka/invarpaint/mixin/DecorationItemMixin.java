68ace22b6daa61fb8784bc20292b006daaa9ac95
Andrew Grant <andrew.grant@mail.rit.edu>
2023-06-21 15:47:12 -0400

    fix formatting

--- src/main/java/tk/estecka/invarpaint/mixin/DecorationItemMixin.java
+++ src/main/java/tk/estecka/invarpaint/mixin/DecorationItemMixin.java
@@ -85,7 +85,8 @@ public class DecorationItemMixin extends Item {
                 return Optional.of(entity);
             else {
                 if (player != null) {
-                    player.sendMessage(Text.translatable("painting.invalid_space", Text.translatable("painting."+variantId.replace(":",".")+".title").formatted(Formatting.YELLOW),
+                    player.sendMessage(Text.translatable("painting.invalid_space",
+                        Text.translatable("painting."+variantId.replace(":",".")+".title").formatted(Formatting.YELLOW),
                         Text.translatable("painting.dimensions", MathHelper.ceilDiv(itemVariant.getWidth(), 16), MathHelper.ceilDiv(itemVariant.getHeight(), 16)).formatted(Formatting.WHITE)),
                         true);
                 }
