f56c9c4e13a8cc54e1cc5b923c27b56da9d8bc40
Estecka <estecka@galacsys.com>
2023-06-22 19:51:01 +0200

    Placing the painting whenever a suitable position is found.

--- src/main/java/tk/estecka/invarpaint/PaintEntityPlacer.java
+++ src/main/java/tk/estecka/invarpaint/PaintEntityPlacer.java
@@ -9,14 +9,10 @@ import org.joml.Vector2i;
 
 import net.minecraft.entity.decoration.painting.PaintingEntity;
 import net.minecraft.entity.decoration.painting.PaintingVariant;
-import net.minecraft.entity.decoration.painting.PaintingVariants;
 import net.minecraft.registry.Registries;
-import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3i;
-import net.minecraft.util.math.Direction.Axis;
 import net.minecraft.world.World;
 
 public class PaintEntityPlacer 
@@ -25,8 +21,8 @@ public class PaintEntityPlacer
 	 * Iterates over the possible positions a paintings could placed on, while still covering the block targeted by the player.
 	 * Iteration is done in a ring-like pattern, so the positions closest to the targeted block are evaluated first.
 	 * 
-	 * Internally, all positions are relative to the top-right corner of the maximum scanned surface,
-	 * but the values returned by the iteration are relative to the targeted block.
+	 * Internally, all positions are relative to the bottom-left corner of the maximum scanned surface,
+	 * but the values returned by the iterators are relative to the targeted block.
 	 */
 	static class	SurfaceIterator implements Iterator<Vector2i>
 	{
@@ -34,9 +30,9 @@ public class PaintEntityPlacer
 		/**
 		 * The position of the targeted block within the scannable surface.
 		 */
-		private final Vector2i startPos;
+		private final Vector2i targetBlock;
 		/**
-		 * Keeps track of which blocks within the scannable surface have already been iterated on.
+		 * Keeps track of which blocks have already been iterated on.
 		 */
 		private final boolean[][] checkList;
 		private Queue<Vector2i> currentRing, nextRing;
@@ -44,7 +40,7 @@ public class PaintEntityPlacer
 		public SurfaceIterator (int variantWidth, int variantHeight){
 			this.width = variantWidth;
 			this.height = variantHeight;
-			this.startPos = new Vector2i(variantWidth/2, variantHeight/2);
+			this.targetBlock = new Vector2i(variantWidth/2, variantHeight/2);
 			this.checkList = new boolean[variantWidth][variantHeight];
 			this.currentRing = new LinkedList<Vector2i>();
 			this.nextRing    = new LinkedList<Vector2i>();
@@ -53,20 +49,20 @@ public class PaintEntityPlacer
 			boolean evenW = (variantWidth %2) == 0;
 			boolean evenH = (variantHeight%2) == 0;
 			
-			currentRing.add(new Vector2i(startPos));
+			currentRing.add(new Vector2i(targetBlock));
 			if (evenW)
-				currentRing.add(new Vector2i(startPos.x-1, startPos.y  ));
+				currentRing.add(new Vector2i(targetBlock.x-1, targetBlock.y  ));
 			if (evenH)
-				currentRing.add(new Vector2i(startPos.x,   startPos.y-1));
+				currentRing.add(new Vector2i(targetBlock.x,   targetBlock.y-1));
 			if (evenW && evenH)
-				currentRing.add(new Vector2i(startPos.x-1, startPos.y-1));
+				currentRing.add(new Vector2i(targetBlock.x-1, targetBlock.y-1));
 			
 			for (Vector2i p : currentRing)
 				checkList[p.x][p.y] = true;
 		}
 
 		/**
-		 * Returns positions adjacents to this one.
+		 * Returns positions adjacent to this one.
 		 * Elements are ordered so as to start from the right, and circle counter-clockwise around the center.
 		 */
 		private Vector2i[]	GetAdjacents(Vector2i v){
@@ -79,10 +75,6 @@ public class PaintEntityPlacer
 			r[i++].add( 0,  1); // ↑
 			r[i++].add(-1,  0); // ←
 			r[i++].add( 0, -1); // ↓
-			// r[i++].add(-1, -1); // ↙
-			// r[i++].add(-1,  1); // ↖
-			// r[i++].add( 1,  1); // ↗
-			// r[i++].add( 1, -1); // ↘
 
 			return r;
 		}
@@ -104,14 +96,13 @@ public class PaintEntityPlacer
 		 * @return	The offset of the position to evaluate, relative to the targeted block.
 		 */
 		public Vector2i	next(){
-			Vector2i pos;
-
 			if (currentRing.isEmpty()){
 				currentRing = nextRing;
 				nextRing = new LinkedList<Vector2i>();
 			}
 
-			pos = currentRing.remove();
+			Vector2i pos = currentRing.remove();
+
 			for (Vector2i adj : GetAdjacents(pos))
 			if (ShouldEvaluate(adj)){
 				nextRing.add(adj);
@@ -119,39 +110,46 @@ public class PaintEntityPlacer
 			}
 
 			Vector2i offset = pos;
-			offset.x -= this.startPos.x;
-			offset.y -= this.startPos.y;
-			return pos;
+			offset.x -= this.targetBlock.x;
+			offset.y -= this.targetBlock.y;
+			return offset;
 		}
 
 	}
 
 	static public Optional<PaintingEntity>	PlaceLockedPainting(World world, BlockPos targetPos, Direction facing, PaintingVariant variant){
-		// PaintingEntity entity = new PaintingEntity(world, targetPos, facing, Registries.PAINTING_VARIANT.getEntry(variant));
-		// if (entity.canStayAttached())
-		// 	return Optional.of(entity);
+		PaintingEntity entity = new PaintingEntity(world, targetPos, facing, Registries.PAINTING_VARIANT.getEntry(variant));
+		if (entity.canStayAttached())
+			return Optional.of(entity);
 
 		// The direction of the horizontal axis of the wall
 		Vec3i right = facing.rotateYCounterclockwise().getVector();
 
-		RegistryEntry<PaintingVariant> debugVariant = Registries.PAINTING_VARIANT.getEntry(PaintingVariants.ALBAN).get();
+		// int i=0;
+		// RegistryEntry<PaintingVariant> debugVariant = Registries.PAINTING_VARIANT.getEntry(PaintingVariants.ALBAN).get();
 
-		int i=0;
 		SurfaceIterator surface = new SurfaceIterator(variant.getWidth()/16, variant.getHeight()/16);
+		surface.next(); // Skip the targeted position, which was already tested.
 		while (surface.hasNext()) {
 			Vector2i planeOffset = surface.next();
-			Vec3i worldOffset = right.multiply(planeOffset.x);
-			worldOffset = worldOffset.offset(Axis.Y, planeOffset.y);
+			Vec3i worldOffset = right.multiply(planeOffset.x).add(0, planeOffset.y, 0);
 
-			
-			BlockPos pos = targetPos.add(worldOffset);
-			if (!world.isClient) {
-				InvariablePaintings.LOGGER.warn("[{}, {}]", planeOffset.x, planeOffset.y);
-				PaintingEntity debugEntity = new PaintingEntity(world, pos, facing, debugVariant);
-				debugEntity.setCustomName(Text.literal("---- " + String.valueOf(i++)));
-				debugEntity.setCustomNameVisible(true);
-				world.spawnEntity(debugEntity);
-			}
+			entity.setPosition(
+				targetPos.getX() + worldOffset.getX(),
+				targetPos.getY() + worldOffset.getY(),
+				targetPos.getZ() + worldOffset.getZ()
+			);
+			if (entity.canStayAttached())
+				return Optional.of(entity);
+
+			// if (!world.isClient) {
+			// 	BlockPos pos = targetPos.add(worldOffset);
+			// 	InvariablePaintings.LOGGER.warn("[{}, {}]", planeOffset.x, planeOffset.y);
+			// 	PaintingEntity debugEntity = new PaintingEntity(world, pos, facing, debugVariant);
+			// 	debugEntity.setCustomName(Text.literal("---- " + String.valueOf(i++)));
+			// 	debugEntity.setCustomNameVisible(true);
+			// 	world.spawnEntity(debugEntity);
+			// }
 		}
 
 		return Optional.empty();
