4f14bb8febaaf458a225b670d3a48f111fee25cb
Estecka <estecka@galacsys.com>
2023-09-22 18:10:17 +0200

    Fixed LootFunction codec

--- src/main/java/tk/estecka/invarpaint/LockVariantRandomlyLootFunction.java
+++ src/main/java/tk/estecka/invarpaint/LockVariantRandomlyLootFunction.java
@@ -1,40 +1,41 @@
 package tk.estecka.invarpaint;
 
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonObject;
-
+import java.util.List;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.condition.LootCondition;
+import net.minecraft.loot.condition.LootConditionTypes;
 import net.minecraft.loot.context.LootContext;
 import net.minecraft.loot.function.ConditionalLootFunction;
 import net.minecraft.loot.function.LootFunctionType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.dynamic.Codecs;
 
 public class LockVariantRandomlyLootFunction 
 extends ConditionalLootFunction
 {
-	static public class Serializer
-	extends ConditionalLootFunction.Serializer<LockVariantRandomlyLootFunction>
-	{
-		@Override
-		public LockVariantRandomlyLootFunction	fromJson(JsonObject json, JsonDeserializationContext ctx, LootCondition[] conditions){
-			return new LockVariantRandomlyLootFunction(conditions);
-		}
+	static public final Codec<LockVariantRandomlyLootFunction> CODEC;
+
+	static {
+		CODEC = RecordCodecBuilder.create(instance -> {
+			return instance.group(Codecs.createStrictOptionalFieldCodec(LootConditionTypes.CODEC.listOf(), "conditions", List.of()).forGetter(conditionalLootFunction -> conditionalLootFunction.conditions)).apply(instance, LockVariantRandomlyLootFunction::new);
+		});
 	}
 
 	static public final LootFunctionType TYPE = Registry.register(
 			Registries.LOOT_FUNCTION_TYPE,
 			new Identifier("lock_variant_randomly"),
-			new LootFunctionType(new LockVariantRandomlyLootFunction.Serializer())
+			new LootFunctionType(CODEC)
 		);
 
 	static public void RegisterFunction(){
 		// Loads the class and initializes static variables
 	};
 
-	public	LockVariantRandomlyLootFunction(LootCondition[] conditions){
+	public	LockVariantRandomlyLootFunction(List<LootCondition> conditions){
 		super(conditions);
 	}
 
