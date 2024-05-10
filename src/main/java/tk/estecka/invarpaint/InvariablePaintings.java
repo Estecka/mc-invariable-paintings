c8efad45672d1517706da2c32b36ce30f9a08fd8
Estecka <estecka@galacsys.com>
2024-05-10 21:06:31 +0200

    Fixed mod initializers

--- src/main/java/tk/estecka/invarpaint/InvariablePaintings.java
+++ src/main/java/tk/estecka/invarpaint/InvariablePaintings.java
@@ -6,12 +6,11 @@ import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import tk.estecka.invarpaint.crafting.FilledPaintingRecipe;
-import tk.estecka.invarpaint.crafting.PaintingReplicationRecipe;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class InvariablePaintings implements ModInitializer
+public class InvariablePaintings
+implements ModInitializer
 {
 	static public final String MODID = "invariablepaintings";
 	static public final Logger LOGGER = LoggerFactory.getLogger("Invar-Paint");
@@ -24,19 +23,11 @@ public class InvariablePaintings implements ModInitializer
 	public void onInitialize() {
 		SellPaintingFactory.Register();
 		LockVariantRandomlyLootFunction.Register();
-		FilledPaintingRecipe.Register();
-		PaintingReplicationRecipe.Register();
 
 		RegisterPack("looting", "Painting Loot", true);
-
-		RegisterPack("crafting-unbound"  , "All Painting Recipes"      , false);
-		RegisterPack("crafting-cloning"  , "Painting Recipe: Cloning"  , false);
-		RegisterPack("crafting-expensive", "Painting Recipe: Expensive", false);
-		RegisterPack("crafting-iterative", "Painting Recipe: Iterative", false);
-		RegisterPack("crafting-recycling", "Painting Recipe: Recycling", false);
 	}
 
-	void RegisterPack(String id, String displayName, boolean defaultEnabled){
+	private void RegisterPack(String id, String displayName, boolean defaultEnabled){
 		final var mod = FabricLoader.getInstance().getModContainer(MODID).get();
 		ResourceManagerHelper.registerBuiltinResourcePack(
 			new Identifier(MODID, id), mod,
