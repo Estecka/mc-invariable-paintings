61f050a624b40bab84b46577874f6f66775b79fd
Estecka <estecka@galacsys.com>
2024-06-18 00:17:47 +0200

    Moved default models to Fabric's Model Loading API

--- src/client/java/tk/estecka/invarpaint/cit/Cits.java
+++ src/client/java/tk/estecka/invarpaint/cit/Cits.java
@@ -1,17 +1,30 @@
 package tk.estecka.invarpaint.cit;
 
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.util.Identifier;
 
 public class Cits
+implements ClientModInitializer, ModelLoadingPlugin
 {
-	static public final ModelIdentifier CIT_MISSING = ModelIdentifier.ofInventoryVariant(Identifier.of("invarpaint", "missing_painting"));
-	static public final ModelIdentifier CIT_FILLED  = ModelIdentifier.ofInventoryVariant(Identifier.of("invarpaint", "filled_painting" ));
-	static public final ModelIdentifier CIT_RANDOM  = ModelIdentifier.ofInventoryVariant(Identifier.of("invarpaint", "random_painting" ));
+	static public final Identifier CIT_MISSING = Identifier.of("invarpaint", "item/missing_painting");
+	static public final Identifier CIT_FILLED  = Identifier.of("invarpaint", "item/filled_painting" );
+	static public final Identifier CIT_RANDOM  = Identifier.of("invarpaint", "item/random_painting" );
 
 	static public final String CIT_PREFIX = "item/painting/";
 
 	static public final ModelIdentifier OfPainting(Identifier variantId){
 		return ModelIdentifier.ofInventoryVariant(variantId.withPrefixedPath(CIT_PREFIX));
+	}
+
+	@Override
+	public void onInitializeClient(){
+		ModelLoadingPlugin.register(this);
+	}
+
+	@Override
+	public void onInitializeModelLoader(Context pluginContext){
+		pluginContext.addModels(CIT_MISSING, CIT_FILLED, CIT_RANDOM);
 	}
 }
