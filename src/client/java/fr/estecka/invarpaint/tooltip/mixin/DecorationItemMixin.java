ee697a37dcde64586dde6672f2d1b33607ca3ac2
Estecka <estecka@galacsys.com>
2025-04-03 23:17:01 +0200

    Recondensed tooltip

--- src/client/java/fr/estecka/invarpaint/tooltip/mixin/DecorationItemMixin.java
+++ src/client/java/fr/estecka/invarpaint/tooltip/mixin/DecorationItemMixin.java
@@ -1,37 +1,48 @@
 package fr.estecka.invarpaint.tooltip.mixin;
 
-import java.util.List;
-import java.util.function.Consumer;
-import net.minecraft.component.type.TooltipDisplayComponent;
+import java.util.Optional;
+import net.minecraft.entity.decoration.painting.PaintingVariant;
 import net.minecraft.item.DecorationItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import fr.estecka.invarpaint.api.PaintStackUtil;
-import fr.estecka.invarpaint.tooltip.TooltipUtil;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+
 
 @Mixin(DecorationItem.class)
 public abstract class DecorationItemMixin 
 {
 
-	// @Inject( method="appendTooltip", at=@At("HEAD") )
-	// public void condenseTooltip(ItemStack stack, Item.TooltipContext context, TooltipDisplayComponent display, Consumer<Text> tooltipAdder, TooltipType type, CallbackInfo ci) {
-	// 	if (stack.isOf(Items.PAINTING) && paint) {
-	// 		Identifier variantId = PaintStackUtil.GetVariantId(stack);
+	@Redirect(
+		method="appendTooltip",
+		at = @At(value="INVOKE", target="net/minecraft/entity/decoration/painting/PaintingVariant.title()Ljava/util/Optional;")
+	)
+	private Optional<Text> RemoveTitle(PaintingVariant variant){
+		return Optional.empty();
+	}
 
-	// 		if (variantId != null || !type.isCreative())
-	// 			TooltipUtil.RemoveOriginalTooltip(tooltip);
+	@Redirect(
+		method="appendTooltip",
+		at = @At(value="INVOKE", target="net/minecraft/entity/decoration/painting/PaintingVariant.author()Ljava/util/Optional;")
+	)
+	private Optional<Text> RemoveAuthor(PaintingVariant variant){
+		return Optional.empty();
+	}
 
-	// 		if (variantId != null)
-	// 			TooltipUtil.AddVariantTooltip(tooltip, variantId, type.isAdvanced());
-	// 	}
-	// }
+
+	@ModifyExpressionValue(
+		method = "appendTooltip",
+		at = @At(value="INVOKE", target="net/minecraft/text/Text.translatable(Ljava/lang/String;[Ljava/lang/Object;)Lnet/minecraft/text/MutableText;")
+	)
+	private MutableText AddCondensedTooltip(MutableText original, @Local RegistryEntry<PaintingVariant> variant) {
+		Optional<Text> author = variant.value().author();
+		if (author.isPresent())
+			original = original.append(Text.literal(" ")).append(author.get());
+		return original;
+	}
 
 }
