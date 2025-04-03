0fa9a456370ace06c8f0d286e093a9efebfbc911
Estecka <estecka@galacsys.com>
2025-04-03 17:14:17 +0200

    Fixed painting placement

--- src/main/java/fr/estecka/invarpaint/core/mixin/DecorationItemMixin.java
+++ src/main/java/fr/estecka/invarpaint/core/mixin/DecorationItemMixin.java
@@ -6,11 +6,9 @@ import net.minecraft.entity.decoration.painting.PaintingVariant;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.DecorationItem;
 import net.minecraft.item.ItemUsageContext;
-import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
@@ -18,7 +16,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import fr.estecka.invarpaint.InvarpaintMod;
 import fr.estecka.invarpaint.core.PaintEntityPlacer;
 import fr.estecka.invarpaint.api.PaintStackUtil;
 import fr.estecka.invarpaint.api.PaintTextUtil;
@@ -29,26 +26,23 @@ public abstract class DecorationItemMixin
 {
 	@WrapOperation( method="useOnBlock", at=@At(value="INVOKE", target="Lnet/minecraft/entity/decoration/painting/PaintingEntity;placePainting(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/Direction;)Ljava/util/Optional;") )
 	private Optional<PaintingEntity> filterPlacedPainting(World world, BlockPos pos, Direction facing, Operation<Optional<PaintingEntity>> original, ItemUsageContext context) {
-		final var registry = world.getRegistryManager().getOrThrow(RegistryKeys.PAINTING_VARIANT);
 		Optional<PaintingEntity> result;
-		String variantName = PaintStackUtil.GetVariantName(context.getStack());
-		Identifier variantId = (variantName==null) ? null : Identifier.tryParse(variantName);
-		RegistryEntry<PaintingVariant> itemVariant = registry.getEntry(variantId).orElse(null);
+		RegistryEntry<PaintingVariant> variantEntry = PaintStackUtil.GetVariantEntry(context.getStack());
 		PlayerEntity player = context.getPlayer();
 
-		if (itemVariant != null) {
-			result = PaintEntityPlacer.PlaceLockedPainting(world, pos, facing, itemVariant);
+		if (variantEntry != null) {
+			result = PaintEntityPlacer.PlaceLockedPainting(world, pos, facing, variantEntry);
 			if (result.isEmpty() && player != null) {
 				player.sendMessage(
 					PaintTextUtil.ServersideTranslatable("painting.invalid_space",
-						PaintTextUtil.TranslatableVariantName(variantId).formatted(Formatting.YELLOW),
-						Text.translatable("painting.dimensions", itemVariant.value().width(), itemVariant.value().height())
+						PaintTextUtil.TranslatableVariantName(variantEntry.getIdAsString()).formatted(Formatting.YELLOW),
+						Text.translatable("painting.dimensions", variantEntry.value().width(), variantEntry.value().height())
 					),
 					true
 				);
 			}
 		}
-		else if (player.isCreative() || (variantName!=null && InvarpaintMod.IsNokebabInstalled()))
+		else if (player.isCreative() /*|| (variantName!=null && InvarpaintMod.IsNokebabInstalled())*/)
 			result = original.call(world, pos, facing);
 		else
 			result = Optional.empty();
