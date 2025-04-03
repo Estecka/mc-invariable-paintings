909f3c53b4cdedd15897b6f163546a557bd1ad6d
Estecka <estecka@galacsys.com>
2025-04-03 20:28:28 +0200

    Monolithic item-state

--- src/client/resources/resourcepacks/vanilla-cit/.build.sh
+++ src/client/resources/resourcepacks/vanilla-cit/.build.sh
@@ -12,3 +12,37 @@ echo >&2 -e "\n\t# models";
 	textures/item/ .png \
 	models/item/   .json \
 	;
+
+echo >&2 -e "\n\t# Monolithic item state";
+
+rm -f .cases
+separator="";
+template=".templates/items-case.json"
+shopt -s globstar
+
+for d in ./assets/*
+do if [[ -d $d ]]
+then
+	export NAMESPACE=$(basename $d)
+	fullPrefix="./assets/$NAMESPACE/textures/item/painting/";
+	srcSuffix=".png";
+	for f in $fullPrefix**/**/*$srcSuffix
+	do if [[ -f $f ]]
+	then
+		export NAME=${f#$fullPrefix}
+		export NAME=${NAME%$srcSuffix}
+
+		dst=".cases"
+		echo >&2 "$NAMESPACE:$NAME";
+		echo -n >>$dst "$separator";
+		envsubst <"$template" >>$dst
+		separator=$',\n'
+	fi
+	done;
+fi
+done;
+
+export CASES=`cat .cases | tr -d $'\r'`;
+envsubst <".templates/items-monolith.json" >"./assets/minecraft/items/painting.json"
+rm .cases
+
